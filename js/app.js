09c57bfdb98a48fded727212647bf98e903f6985
Kanagamani Dhass <kanagamanidhass200022@gmail.com>
2021-08-19 16:44:00 +0530

    Second CHanges

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-const unityInstance = UnityLoader.instantiate("unityContainer", "Build/WebAR_Markers_new.json");
+const unityInstance = UnityLoader.instantiate("unityContainer", "Build/MultiMarker.json");
 let isCameraReady = false;
 let isDetectionManagerReady = false;
 let gl = null;
